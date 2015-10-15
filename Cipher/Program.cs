a132e8d16708e47996bfba2b27baef6ff0050996
Ashley Hauck <echauck@mtu.edu>
2015-10-15 17:51:23 -0400

    Initial commit

---
+++ Cipher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cipher
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World from Team Cipher's Encrypted Messaging Service!");
+		}
+	}
+}
