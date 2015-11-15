bd4197a3bcac478d19b627c0f8b02fa2c0fbabd3
Mark Schlax <mcschlax@mtu.edu>
2015-11-15 12:56:32 -0500

    GUI is ready for contact implementation

--- Cipher/Gui.cs
+++ Cipher/Gui.cs
@@ -40,36 +40,99 @@ namespace Cipher
         public Gui(string name)
         {
             Client client = null;
-            var window = new Form();
-            window.Text = "Hello, world!";
-            window.Size = new Size(400, 600);
+
+            Form window = new Form();
+            Panel chatInputPanel = new Panel();
+            Panel chatPanel = new Panel();
+            RichTextBox chatBox = new RichTextBox();
+            Panel inputSendPanel = new Panel();
+            Button sendButton = new Button();
+            RichTextBox inputBox = new RichTextBox();
+            Panel contactPanel = new Panel();
+
+            window.SuspendLayout();
+            chatInputPanel.SuspendLayout();
+            chatPanel.SuspendLayout();
+            inputSendPanel.SuspendLayout();
+            contactPanel.SuspendLayout();
+
+            chatInputPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            chatInputPanel.Controls.Add(inputSendPanel);
+            chatInputPanel.Controls.Add(chatPanel);
+            chatInputPanel.Location = new Point(0, 0);
+            chatInputPanel.Name = "ChatInputPanel";
+            chatInputPanel.Size = new Size(400, 600);
+            chatInputPanel.TabIndex = 0;
+
+            chatPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            chatPanel.Controls.Add(chatBox);
+            chatPanel.Location = new Point(0, 0);
+            chatPanel.Name = "ChatPanel";
+            chatPanel.Size = new Size(400, 500);
+            chatPanel.TabIndex = 1;
+
+            chatBox.BackColor = SystemColors.Window;
+            chatBox.Dock = DockStyle.Fill;
+            chatBox.Location = new Point(0, 0);
+            chatBox.Name = "ChatBox";
+            chatBox.ReadOnly = true;
+            chatBox.Size = new Size(400, 500);
+            chatBox.TabIndex = 2;
+            chatBox.Text = "";
+
+            inputSendPanel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            inputSendPanel.Controls.Add(sendButton);
+            inputSendPanel.Controls.Add(inputBox);
+            inputSendPanel.Location = new Point(0, 500);
+            inputSendPanel.Name = "InputSendPanel";
+            inputSendPanel.Size = new Size(400, 100);
+            inputSendPanel.TabIndex = 1;
+
+            inputBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            inputBox.Location = new Point(0, 0);
+            inputBox.Name = "InputBox";
+            inputBox.Size = new Size(300, 100);
+            inputBox.TabIndex = 2;
+            inputBox.Text = "";
+
+            sendButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            sendButton.Location = new Point(300, 0);
+            sendButton.Name = "SendButton";
+            sendButton.Size = new Size(100, 100);
+            sendButton.TabIndex = 2;
+            sendButton.Text = "Send";
+            sendButton.UseVisualStyleBackColor = true;
+            sendButton.Click += (sender, args) =>
+            {
+                // TODO: Actually implement this
+                client.SendMessage(client.LookUpUser("alice").Single(), inputBox.Text);
+                inputBox.Text = "";
+            };
+
+            contactPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            contactPanel.Location = new Point(400, 0);
+            contactPanel.Name = "ContactPanel";
+            contactPanel.Size = new Size(200, 600);
+            contactPanel.TabIndex = 0;
+
+            window.AutoScaleDimensions = new SizeF(12F, 25F);
+            window.AutoScaleMode = AutoScaleMode.Font;
+            window.ClientSize = new Size(600, 600);
+            window.Controls.Add(chatInputPanel);
+            window.Controls.Add(contactPanel);
+            window.Name = "Window";
+            window.Text = "CipherEMS";
             window.FormClosed += (o, e) =>
             {
                 Application.Exit();
             };
 
-            var chatBox = new TextBox();
-            chatBox.ReadOnly = true;
-            chatBox.Anchor = AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left;
-            chatBox.WordWrap = true;
-            chatBox.ScrollBars = ScrollBars.Vertical;
-            window.Controls.Add(chatBox);
+            contactPanel.ResumeLayout(false);
+            inputSendPanel.ResumeLayout(false);
+            chatPanel.ResumeLayout(false);
+            chatInputPanel.ResumeLayout(false);
+            window.ResumeLayout(false);
 
-            var textBox = new TextBox();
-            textBox.WordWrap = true;
-            chatBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            window.Controls.Add(textBox);
-
-            var button = new Button();
-            button.Text = "Send";
-            button.Click += (sender, args) =>
-            {
-                // TODO: Actually implement this
-                client.SendMessage(client.LookUpUser("alice").Single(), textBox.Text);
-                textBox.Text = "";
-            };
-            window.Controls.Add(button);
-            
             string server = Config.Get("Server", "71.13.216.7");
             int port = Config.Get("Port", 60100, int.TryParse);
             client = new Client(server, port, new EncryptionService(), name, () =>
